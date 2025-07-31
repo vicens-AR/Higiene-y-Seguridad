e091bbb9748564d8b3f71b1e0b1c95ae52678178
unknown <vicente>
2025-07-31 19:22:07 -0300

    jejejuju

--- app/app.py
+++ app/app.py
@@ -14,6 +14,10 @@ db = SQLAlchemy(app)
 def index():
     return render_template('index.html')
 
+@app.route('/Form')
+def form():
+    return render_template('form.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
 
